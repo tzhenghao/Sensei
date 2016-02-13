73830439b60b8a1bd919e7e32812a588ee51f694
tzhenghao <tanzhao@umich.edu>
2016-02-12 19:25:26 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Sensei
+
+## Introduction
 A Google Now terminal program that prints out weather forecasts and emails by fetching from other Python scripts.
+
---
+++ sensei.py
@@ -0,0 +1,16 @@
+def printWeather():
+
+# EFFECTS: Reads out the weather forecast for the day
+def speakWeather():
+
+# EFFECTS: Prints out the content of the email.
+def printEmail():
+
+# EFFECTS: Reads out the content of the email.
+def speakEmail():
+
+def listen():
+
+def printDailyHeadlines():
+
+def speakDailyHeadlines():
